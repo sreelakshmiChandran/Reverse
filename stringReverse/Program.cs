df7602c1e71e60964be8f827f5eadd3997356696
sreelakshmiChandran <sreelakshmichandran137@gmail.com>
2023-02-15 14:03:49 +0530

    Second commit

--- stringReverse/Program.cs
+++ stringReverse/Program.cs
@@ -1,5 +1,6 @@
 ﻿Console.WriteLine("Enter the string: ");
 string name = Console.ReadLine();
+Console.WriteLine("The string is: " + name);
 Console.WriteLine("Reverse of the string: ");
 for (int i = name.Length - 1; i >= 0; i--)
 {
